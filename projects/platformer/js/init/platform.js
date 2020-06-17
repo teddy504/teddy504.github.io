620c0ad8198b375fe2606d3406d0eb410d850dc2
Ubuntu <ubuntu@ip-172-31-57-166.ec2.internal>
2020-06-17 20:50:14 +0000

    added portfolio

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,56 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+
+        // example:
+        createPlatform(400, 250);
+        
+        createPlatform(400, 460);
+        
+        createPlatform(300, 200, 0.3)
+        
+        createPlatform(500, 500, 0.3, 10)
+        
+        createPlatform(250, 560)      
+        
+         createPlatform(200, 625, 0.3)
+        // small horizontal platform (30% the normal width)
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
